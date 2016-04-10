208e944469ce81e2146a39b90fc574e8d1ae820c
Isaac Kwon <minals3165@gmail.com>
2016-04-10 16:46:07 +0900

    Paralell Optimization

--- WaveFunctionSimulation/diffractionFunctionplot2.m
+++ WaveFunctionSimulation/diffractionFunctionplot2.m
@@ -6,9 +6,9 @@ function [] = diffractionFunctionplot2(wavelength, slitx, Y)
 % wavelength = 2
 % slitx = 5
 
-divide = 1000;  %set the Resolution of graph
-slitdivide = 200;%10^11; %set the Resolution of Numerical Integration
-angledivide = 64;
+divide = 256;  %set the Resolution of graph
+slitdivide = 256;%10^11; %set the Resolution of Numerical Integration
+angledivide = 256;
 
 A = 1;
 k = 2*pi/wavelength;
@@ -17,7 +17,7 @@ wavefunc = @(r,t) A*cos(k*r+t)./r;
 EucDM = @(r1x, r1y, r2x, r2y) sqrt( (r1x - r2x).^2 + (r1y - r2y).^2 ) ;
 
 
-xrange = [0,5];
+xrange = [0,2];
 
 xrange = linspace(xrange(1),xrange(2),divide);
 
@@ -26,20 +26,18 @@ integraldivide = slitdivide;  %slitx*slitdivide; %slit divide
 slit = [mean(xrange)-(slitx/2), mean(xrange)+(slitx/2)];
 slitrange = linspace(slit(1), slit(2), integraldivide);
 
-values = zeros(size(xrange,2),integraldivide);
 
 
+angle = linspace(0,2*pi,angledivide);
 
-for k = 1:angledivide
-    
-    for i = 1:integraldivide
-        values(:,i,k) = wavefunc( EucDM(xrange, Y, slitrange(i),0 ) , 2*pi*(k-1)/angledivide);
+%values = [];%zeros([size(xrange,2) size(slitrange,2) size(angle,2)]);
 
-        A = A/slitx;
+[Xrange, S, Ag] = ndgrid(xrange,  slitrange, angle);
 
-    end
+clearvars -except wavefunc EucDM Xrange xrange Y S Ag wavelength slitx divide slitdivide angledivide integraldivide
 
-end
+values = wavefunc( EucDM(Xrange, Y, S ,0) , Ag);
+
 % figure
 % 
 % surf(values)
@@ -52,17 +50,13 @@ values = abs((sum(values,2)/integraldivide));
 
 values = (sum(values,3)).^2;
 
-
-
-% figure
-
 plot( xrange ,values)
 
 
 
 
 pt1 = ['Wavelength : ', num2str(wavelength),'    Slit Size : ',  num2str(slitx)];
-pt2 = ['RangeDivide: ', num2str(divide),'     SlitDivide : ',num2str(slitdivide)];
+pt2 = ['RangeDivide: ', num2str(divide),'     SlitDivide : ',num2str(slitdivide),'     AngleDivide : ',num2str(angledivide)];
 title({pt1; pt2});
 
 end
