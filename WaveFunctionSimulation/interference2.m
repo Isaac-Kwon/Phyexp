0081f2856fb48dd253ebeec2bd1b651dbc56ebec
Isaac Kwon <minals3165@gmail.com>
2016-04-25 20:21:50 +0900

    Fhelper setting

--- WaveFunctionSimulation/interference2.m
+++ WaveFunctionSimulation/interference2.m
@@ -4,17 +4,13 @@ slitd = 0.25*10^-3; %gap of slit
 pxrange = 38*10^-3; %plotting range (xrange)
 Y = 1.405; %distance to screen
 
-figure
 
 hold on
 
 p1 = interferenceFunctionplot2(wavelength, slita, slitd, pxrange, Y);
 
-p2 = interferenceFunctionplot2(wavelength, slita, slitd*2, pxrange, Y);
-
-p3 = interferenceFunctionplot2(wavelength, slita, slitd/2, pxrange, Y);
-
-
 hold off
 
-legend([p1,p2,p3], '1','*2','/2')
+%legend([p1,p2,p3], '1','*2','/2')
+xlabel('Position (mm)');
+ylabel('Intensity');
