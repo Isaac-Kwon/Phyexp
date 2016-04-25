6d9cc4ff65b69a71839bd073f5fcd9740355f18a
Isaac Kwon <minals3165@gmail.com>
2016-04-26 00:23:17 +0900

    Minor Update

--- WaveFunctionSimulation/interference2.m
+++ WaveFunctionSimulation/interference2.m
@@ -4,7 +4,6 @@ slitd = 0.25*10^-3; %gap of slit
 pxrange = 38*10^-3; %plotting range (xrange)
 Y = 1.405; %distance to screen
 
-
 hold on
 
 p1 = interferenceFunctionplot2(wavelength, slita, slitd, pxrange, Y);
@@ -12,5 +11,3 @@ p1 = interferenceFunctionplot2(wavelength, slita, slitd, pxrange, Y);
 hold off
 
 %legend([p1,p2,p3], '1','*2','/2')
-xlabel('Position (mm)');
-ylabel('Intensity');
